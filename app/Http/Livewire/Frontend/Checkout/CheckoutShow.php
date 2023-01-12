6a7a7c631e7fc1505537769f066e71f7293799fb
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-12 14:51:19 +0500

    order and orderitems create database successfuly

--- app/Http/Livewire/Frontend/Checkout/CheckoutShow.php
+++ app/Http/Livewire/Frontend/Checkout/CheckoutShow.php
@@ -3,11 +3,80 @@
 namespace App\Http\Livewire\Frontend\Checkout;
 
 use App\Models\Card;
+use App\Models\Order;
 use Livewire\Component;
+use App\Models\Orderitems;
+use Illuminate\Support\Str;
 
 class CheckoutShow extends Component
 {
-    public $cards, $totalProductAmout;
+    public $cards, $totalProductAmout = 0;
+
+    public $email, $fullname, $phone, $pincode, $address, $payment_mode = NULL, $payment_id = NULL;
+
+    public function rules(){
+        return [
+            'fullname' => 'required|string|max:121',
+            'email' => 'required|email|max:121',
+            'phone' => 'required|string|max:20|min:5',
+            'pincode' => 'required|string|max:6|min:6',
+            'address' => 'required|string|max:500',
+        ];
+    }
+
+    public function placeOrder() {
+
+        $this->validate();
+        $order = Order::create([
+            'user_id' => auth()->user()->id,
+            'tracking_no' => 'funda-'.Str::random(10),
+            'fullname' => $this->fullname,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'pincode' => $this->pincode,
+            'address' => $this->address,
+            'status_message' => 'in progress',
+            'payment_mode' => $this->payment_mode,
+            'payment_id' => $this->payment_id,
+        ]);
+
+        foreach($this->cards as $item) {
+
+            $orderItems = Orderitems::create([
+                'order_id' => $order->id,
+                'product_id' => $item->product_id,
+                'product_color_id' => $item->product_color_id,
+                'quantity' => $item->quantity,
+                'price' => $item->product->selling_price,
+            ]);
+
+        }
+
+    }
+
+    public function codOrder() {
+        $this->payment_mode = 'Cash on Delivery';
+
+        $placeOrder = $this->placeOrder();
+
+        if($placeOrder) {
+
+            Card::where('user_id', auth()->user()->id)->delete();
+
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Order placed successfully',
+                'type' => 'success',
+                'status' => 200
+            ]);
+        } else {
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'somtsing were wrong',
+                'type' => 'error',
+                'status' => 500 
+            ]);
+        }
+
+    }
 
     public function totalproductAmout() {
 
@@ -21,6 +90,8 @@ class CheckoutShow extends Component
 
     public function render()
     {
+        $this->fullname = auth()->user()->name;
+        $this->email = auth()->user()->email;
         $this->totalProductAmout = $this->totalproductAmout();
         return view('livewire.frontend.checkout.checkout-show',[
             'totalProductAmout' => $this->totalProductAmout
--- app/Models/Orderitems.php
+++ app/Models/Orderitems.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order_items extends Model
+class Orderitems extends Model
 {
     use HasFactory;
 
@@ -18,5 +18,4 @@ class Order_items extends Model
         'quantity',
         'price',
     ];
-
 }
