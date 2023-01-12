5b93e7035731f41e449229bfa925475d961793fb
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-12 13:22:12 +0500

    Order model create successfully

--- database/migrations/2023_01_12_075356_create_order_items_table.php
+++ database/migrations/2023_01_12_075356_create_order_items_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->integer('order_id');
             $table->integer('product_id');
-            $table->integer('product_color_id');
+            $table->integer('product_color_id')->nullable();
             $table->integer('quantity');
             $table->integer('price');
             $table->timestamps();
