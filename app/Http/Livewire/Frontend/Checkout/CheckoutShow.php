f90a1537587e04c1e3a4dc8614ccb3ff36b85a3c
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-12 12:38:09 +0500

    totalProductAmout create successfully

--- app/Http/Livewire/Frontend/Checkout/CheckoutShow.php
+++ app/Http/Livewire/Frontend/Checkout/CheckoutShow.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Livewire\Frontend\Checkout;
 
+use App\Models\Card;
 use Livewire\Component;
 
 class CheckoutShow extends Component
 {
+    public $cards, $totalProductAmout;
+
+    public function totalproductAmout() {
+
+        $this->cards = Card::where('user_id', auth()->user()->id)->get();
+        foreach($this->cards as $item){
+            $this->totalProductAmout += $item->product->selling_price * $item->quantity;
+        }
+        return $this->totalProductAmout;
+
+    }
+
     public function render()
     {
-        return view('livewire.frontend.checkout.checkout-show');
+        $this->totalProductAmout = $this->totalproductAmout();
+        return view('livewire.frontend.checkout.checkout-show',[
+            'totalProductAmout' => $this->totalProductAmout
+        ]);
     }
 }
