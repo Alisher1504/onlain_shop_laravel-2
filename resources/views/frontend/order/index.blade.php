b8e8541fed6973aea95ceaa79665b7c14e622772
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-14 10:36:53 +0500

    Order view page create successfully

---
+++ resources/views/frontend/order/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="py-3 py-md-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="shadow bg-white p-3">
+                        <h4 class="mb-4">My Orders</h4>
+                        <hr>
+                        <div class="table-responsive">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
