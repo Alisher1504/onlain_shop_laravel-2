a46b229bf5c678139fff4dc7bbc22a9db3a980d7
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-14 11:41:54 +0500

    Order view add successfully

--- app/Http/Controllers/Frontend/OrderController.php
+++ app/Http/Controllers/Frontend/OrderController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
     public function index() {
-        return view('frontend.order.index');
+
+        $order = Order::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->paginate(5);
+        return view('frontend.order.index', compact('order'));
     }
 }
--- resources/views/frontend/order/index.blade.php
+++ resources/views/frontend/order/index.blade.php
@@ -9,7 +9,47 @@
                         <h4 class="mb-4">My Orders</h4>
                         <hr>
                         <div class="table-responsive">
-                            
+
+                            <table class="table table-bordered table-striped">
+
+                                <thead>
+                                    <tr>
+                                        {{-- <th>Order id</th> --}}
+                                        <th>Trasking no</th>
+                                        <th>user Name</th>
+                                        <th>Paymrnt Made</th>
+                                        <th>Ordered Date</th>
+                                        <th>Status message</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    @forelse ($order as $item)
+                                        <tr>
+                                            {{-- <td>{{ $item->id }}</td> --}}
+                                            <td>{{ $item->tracking_no }}</td>
+                                            <td>{{ $item->fullname }}</td>
+                                            <td>{{ $item->payment_mode }}</td>
+                                            <td>{{ $item->created_at->format('d-m-y') }}</td>
+                                            <td>{{ $item->status_message }}</td>
+                                            <td>
+                                                <a href="{{ 'orders/'.$item->id }}" class="btn btn-primary">View</a>
+                                            </td>
+                                        </tr>
+                                    @empty  
+                                        <tr>
+                                            <td colspan="7">NoOrders Aviable</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+
+                            </table>
+
+                            <div>
+                                {{ $order->links() }}
+                            </div>
+
                         </div>
                     </div>
                 </div>
