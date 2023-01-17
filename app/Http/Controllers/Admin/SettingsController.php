b3679442d42b8e0f79b682f956561b652facf3de
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-17 13:34:38 +0500

    setting update successfully

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -11,7 +11,8 @@ class SettingsController extends Controller
     
     public function index() {
 
-        return view('admin.settings.index');
+        $settings = Settings::first();
+        return view('admin.settings.index', compact('settings'));
 
     }
 
@@ -20,6 +21,27 @@ class SettingsController extends Controller
         $settings = Settings::first();
 
         if($settings) {
+
+            $settings->update([
+                'website_name' => $request->website_name,
+                'website_url' => $request->website_url,
+                'page_title' => $request->page_title,
+                'meta_keyvords' => $request->meta_keyvords,
+                'meta_description' => $request->meta_description,
+
+                'address' => $request->address,
+                'phone1' => $request->phone1,
+                'phone2' => $request->phone2,
+                'email1' => $request->email1,
+                'email2' => $request->email2,
+
+                'facebook' => $request->facebook,
+                'telegram' => $request->telegram,
+                'instagram' => $request->instagram,
+                'youtube' => $request->youtube,
+            ]);
+
+            return redirect()->back()->with('message', 'Setting update successfully');
 
         } else {
 
--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -22,25 +22,25 @@
                         <div class="row">
                             <div class="col-md-6 my-3 input-group">
                                 <span class="input-group-text">Website name</span>
-                                <input type="text" name="website_name" value="{{  }}" class="form-control">
+                                <input type="text" name="website_name" value="{{ $settings->website_name ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text">Website url</span>
-                                <input type="text" name="website_url" class="form-control">
+                                <input type="text" name="website_url" value="{{ $settings->website_url ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-12 mb-3 input-group">
                                 <span class="input-group-text">Page title</span>
-                                <input type="text" name="page_title" class="form-control">
+                                <input type="text" name="page_title" value="{{ $settings->page_title ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
-                                <textarea name="meta_keyvords" rows="3" class="form-control" placeholder="Meta keyvord"></textarea>
+                                <textarea name="meta_keyvords" rows="3" class="form-control" placeholder="Meta keyvord">{{ $settings->meta_keyvords ?? '' }}</textarea>
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
-                                <textarea name="meta_description" rows="3" class="form-control" placeholder="Meta description"></textarea>
+                                <textarea name="meta_description" rows="3" class="form-control" placeholder="Meta description">{{ $settings->meta_description ?? '' }}</textarea>
                             </div>
                         </div>
                     </div>
@@ -54,27 +54,27 @@
                         <div class="row">
 
                             <div class="col-md-12 my-3 input-group">
-                                <textarea name="address" rows="3" class="form-control" placeholder="Address"></textarea>
+                                <textarea name="address" rows="3" class="form-control" placeholder="Address">{{ $settings->address ?? '' }}</textarea>
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text">Phone 1</span>
-                                <input type="text" name="phone1" class="form-control">
+                                <input type="text" name="phone1" value="{{ $settings->phone1 ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text">Phone No 2</span>
-                                <input type="text" name="phone2" class="form-control">
+                                <input type="text" name="phone2" value="{{ $settings->phone2 ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text">Email 1</span>
-                                <input type="text" name="email1" class="form-control">
+                                <input type="text" name="email1" value="{{ $settings->email1 ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text">Email Id 2</span>
-                                <input type="text" name="email2" class="form-control">
+                                <input type="text" name="email2" value="{{ $settings->email2 ?? '' }}" class="form-control">
                             </div>
                         </div>
                     </div>
@@ -89,22 +89,22 @@
 
                             <div class="col-md-6 my-3 input-group">
                                 <span class="input-group-text"><i class="bi bi-facebook fs-5"></i></span>
-                                <input type="text" name="facebook" class="form-control">
+                                <input type="text" name="facebook" value="{{ $settings->facebook ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text"><i class="bi bi-telegram fs-5"></i></span>
-                                <input type="text" name="telegram" class="form-control">
+                                <input type="text" name="telegram" value="{{ $settings->telegram ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text"><i class="bi bi-instagram fs-5"></i></span>
-                                <input type="text" name="instagram" class="form-control">
+                                <input type="text" name="instagram" value="{{ $settings->instagram ?? '' }}" class="form-control">
                             </div>
 
                             <div class="col-md-6 mb-3 input-group">
                                 <span class="input-group-text"><i class="bi bi-youtube fs-5"></i></span>
-                                <input type="text" name="youtube" class="form-control">
+                                <input type="text" name="youtube" value="{{ $settings->youtube ?? '' }}" class="form-control">
                             </div>
                         </div>
                     </div>
