f64aef78f44dcb5acb07887b787055fc91d59d40
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-17 13:41:50 +0500

    View::share() view all settings create successfully

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Settings;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        $settings = Settings::first();
+        View::share('settingsvalue', $settings);
+
     }
 }
