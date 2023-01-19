17bf849ed6340de1de6098343a3385942e24bced
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-19 20:16:04 +0500

    all page send admin dashboard successfully

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,15 +2,36 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Catagory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
     
     public function index() {
 
-        return view('admin.index');
+        $product = Product::count();
+        $category = Catagory::count();
+
+        $totalUser = User::count();
+        $user = User::where('role_as', '0')->count();
+        $admin = User::where('role_as', '1')->count();
+
+        $todaydate = Carbon::now()->format('d-m-y');
+        $todaymon = Carbon::now()->format('m');
+        $todayyear = Carbon::now()->format('y');
+
+        $totalorder = Order::count();
+        $todayOrder = Order::where('created_at', $todaydate)->count();
+        $monOrder = Order::where('created_at', $todaymon)->count();
+        $yearOrder = Order::where('created_at', $todayyear)->count();
+
+        return view('admin.index', compact('product', 'category','totalUser', 'user', 'admin', 'totalorder', 'todayOrder', 'monOrder', 'yearOrder'));
 
     }
 
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,15 +3,30 @@
 @section('content')
     <main id="main" class="main">
 
-            @if (session('status'))
-                <div class="container bg-info bordered text-center my-3">
-                    <h2 class="py-3 text-white">{{ session('status') }}</h2>
-                </div>
-            @endif
+        @if (session('status'))
+        <div class="container bg-info bordered text-center my-3">
+            <h2 class="py-3 text-white">{{ session('status') }}</h2>
+        </div>
+        @endif
 
-        <section class="section dashboard">
-            
-        </section>
+        <div class="me-md-3 me-xl-5">
+
+            <h2>Dashboard</h2>
+            <p>You analitic dashboard template</p>
+            <hr>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white mb-3">
+                    <h1>4</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+        </div>
 
     </main>
 @endsection
