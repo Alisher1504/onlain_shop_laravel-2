c7b369fa572f378734b72d94962e40fd25507abb
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-19 20:22:57 +0500

    admin dashboard view all example

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -21,7 +21,84 @@
 
             <div class="col-md-3">
                 <div class="card card-body bg-primary text-white mb-3">
-                    <h1>4</h1>
+                    <h1>Total Order</h1>
+                    <h1>{{ $totalorder }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white mb-3">
+                    <h1>Today Order</h1>
+                    <h1>{{ $todayOrder }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-warning text-white mb-3">
+                    <h1>This Month Order</h1>
+                    <h1>{{ $monOrder }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-danger text-white mb-3">
+                    <h1>This Year Order</h1>
+                    <h1>{{ $yearOrder }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+        </div>
+
+        <hr>
+
+        <div class="row">
+
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white mb-3">
+                    <h1>Total Product</h1>
+                    <h1>{{ $product }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white mb-3">
+                    <h1>Today Category</h1>
+                    <h1>{{ $category }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+        </div>
+
+        <hr>
+
+        <div class="row">
+
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white mb-3">
+                    <h1>Total All User</h1>
+                    <h1>{{ $totalUser }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white mb-3">
+                    <h1>Total User</h1>
+                    <h1>{{ $user }}</h1>
+                    <a href="{{ url('admin/orders') }}" class="text-white">view</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-dark text-white mb-3">
+                    <h1>Today Admin</h1>
+                    <h1>{{ $admin }}</h1>
                     <a href="{{ url('admin/orders') }}" class="text-white">view</a>
                 </div>
             </div>
