2f7f4a87bd7a07ea3acf5a8a2881966896c859f8
Alisher1504 <ruzmetov150499@gmail.com>
2023-01-19 10:56:39 +0500

    Users view page create successfully

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\User;
 
 class UsersController extends Controller
 {
-    //
+    public function index() {
+
+        $users = User::paginate(10);
+        return view('admin.users.index', compact('users'));
+
+    }
 }
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.admin')
+
+@section('content')
+<main id="main" class="main">
+
+    <div class="row">
+        <div class="col-md-12">
+
+            @if (session('status'))
+            <div class="container bg-info bordered text-center my-3">
+                <h2 class="py-3 text-white">{{ session('status') }}</h2>
+            </div>
+            @endif
+
+            <div class="card">
+                <div class="card-header">
+
+                    <h3>
+                        Users
+
+                        <a href="{{ url('admin/users/create') }}" class="btn btn-primary btn-sm float-end">add
+                            Users</a>
+
+                    </h3>
+
+                </div>
+                <div class="card-body">
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Role_as</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $item)
+                            <tr>
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->email }}</td>
+                                <td>
+                                    @if ($item->role_as == '0')
+                                        <label class="btn btn-primary">User</label>
+                                    @elseif ($item->role_as == '1')
+                                        <label class="btn btn-success">Admin</label>
+                                    @else
+                                        <label class="btn btn-danger">none</label>
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="{{ url('admin/product/edit/'. $item->id) }}"
+                                        class="btn btn-success">Edit</a>
+                                </td>
+                                <td>
+                                    <a href="{{ url('admin/product/delete/'. $item->id) }}"
+                                        class="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+@endsection
